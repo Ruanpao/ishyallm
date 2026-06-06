69a662debfdb2f759992350655f22b68a443ed41
Ruanpao <ltcdz114514@gmail.com>
2026-06-06 21:33:56 +0800

    创建

---
+++ src/main/java/ruanpao/ishyallm/IshyallmApplication.java
@@ -0,0 +1,13 @@
+package ruanpao.ishyallm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IshyallmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IshyallmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ruanpao/ishyallm/IshyallmApplicationTests.java
@@ -0,0 +1,13 @@
+package ruanpao.ishyallm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshyallmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
